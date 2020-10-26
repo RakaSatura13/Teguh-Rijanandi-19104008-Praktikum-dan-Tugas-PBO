df1ba1bbbfd4f2c5cbf0ed741b4e5e158b07c91e
Teguh Rijanandi <teguhrijanandi02@gmail.com>
2020-10-27 06:52:11 +0700

    Revert "First commit"

--- Modul 2/src/com/Teguh/Modul2/Overriding/X.java
+++
@@ -1,9 +0,0 @@
-package com.Teguh.Modul2.Overriding;
-
-// Class X
-public class X {
-    // Method getValue
-    public void getValue(String value){
-        System.out.println("Value class X\t: " + value);
-    }
-}
